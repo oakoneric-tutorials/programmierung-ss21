4eaf67bf2b061765f02ec0072592675a73c4d0a1
eric <k-eric@outlook.com>
2021-04-13 12:47:43 +0200

    delete .listing files

--- create_slides.py
+++ create_slides.py
@@ -52,6 +52,7 @@ os.remove(handoutFilename + '.out')
 os.remove(handoutFilename + '.snm')
 os.remove(handoutFilename + '.toc')
 os.remove(handoutFilename + '.vrb')
+os.remove(handoutFilename + '.listing')
 os.remove(tempFilename + texExt)
 # =============================================================================
 
@@ -78,6 +79,7 @@ os.remove(slidesFilename + '.out')
 os.remove(slidesFilename + '.snm')
 os.remove(slidesFilename + '.toc')
 os.remove(slidesFilename + '.vrb')
+os.remove(slidesFilename + '.listing')
 os.remove(tempFilename + texExt)
 # =============================================================================
 
